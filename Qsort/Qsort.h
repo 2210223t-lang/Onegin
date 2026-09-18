755d81ccd4b7d82dd4f3f64a7027ad2a8feb414e
Nss <2210223t@gmail.com>
2026-09-18 23:08:59 +0300

    Refactoring

---
+++ Qsort/Qsort.h
@@ -0,0 +1,7 @@
+#ifndef QSORT_H
+#define QSORT_H
+#include "stdio.h"
+
+int my_qsort( void* array, int nElem, size_t szElem, bool ( *Comp )( const void*, const void* ) );
+
+#endif // QSORT_H
---
+++ Qsort/textfunc.h
@@ -0,0 +1,6 @@
+#ifndef TEXTFUNC_H
+#define TEXTFUNC_H
+
+void ReadText( char **ind, size_t szInd, FILE* text );
+
+#endif // TEXTFUNC_H
